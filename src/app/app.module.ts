fcb16944965a1b128bee192b64ed6683868f6c41
Erik Hoffman <erik@MacBook-Pro.local>
2019-05-15 12:14:31 -0400

    Added Component Files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipiesListComponent } from './recipe-book/recipies-list/recipies-list.component';
+import { RecipiesDetailsComponent } from './recipe-book/recipies-details/recipies-details.component';
+import { RecipeitemComponent } from './recipe-book/recipies-list/recipeitem/recipeitem.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeBookComponent,
+    ShoppingListComponent,
+    HeaderComponent,
+    RecipiesListComponent,
+    RecipiesDetailsComponent,
+    RecipeitemComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
