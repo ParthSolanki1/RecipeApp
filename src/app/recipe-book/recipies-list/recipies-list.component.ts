365d742848cbbc82408e170d5a97bfc913327286
Erik Hoffman <erik@MacBook-Pro.local>
2019-05-15 15:06:53 -0400

    Created Recipe creation looking system

--- src/app/recipe-book/recipies-list/recipies-list.component.ts
+++ src/app/recipe-book/recipies-list/recipies-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipies-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipies-list.component.css']
 })
 export class RecipiesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("A Test Recipe", "This is simply a test", "https://www.wellplated.com/wp-content/uploads/2017/12/Hoppin-John-recipe-600x629.jpg")
+  ];
 
   constructor() { }
 
